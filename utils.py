2b818160c08c334dae7c101398fb7e315ad546fc
Atta Mirza <atta.mirza@outlook.de>
2022-05-17 21:33:56 +0200

    seperated recognizing termux from linux

--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
 from config import windows, linux, termux
-import platform
+import platform, os
 
 def get_operatingSystem():
     if platform.system() == "Windows":
         return windows
-    if platform.system() == "Linux":
+    elif platform.system() == "Linux" and os.getcwd().find("termux")!=-1:
         return termux
+    elif platform.system() == "Linux":
+        return linux 
+    else:
+        raise ValueError("COULD NOT RECOGNIZE OS")
     return None
